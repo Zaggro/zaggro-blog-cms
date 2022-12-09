1125eb7fb115bd8f0e445f253cda0862add3f42e
Georg-Gert <georg.gert.tammeleht@gmail.com>
2022-12-09 19:26:08 +0200

    Add background images

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Login from 'views/Login/Login'
+import styles from './App.module.scss'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">App</div>
-    </BrowserRouter>
+    <div className={styles.root}>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/articles/:id/edit" element={<Edit />} />
+        <Route path="/articles/:id/preview" element={<Preview />} />
+        <Route path="/articles" element={<List />} /> 
+  <Route path="/login" element={<Login />} /> */}
+          <Route path="/" element={<Navigate replace to="login" />} />
+          <Route path="*" element={<Navigate replace to="login" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
